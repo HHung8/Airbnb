a47263b0b03811670c7de80d3c65f1d84d0a15e3
Nguyễn Hưng <nguyenhung@MacBook-Air-cua-Nguyen.local>
2024-03-10 05:00:16 +0700

    add file modifile

--- app/actions/getFavoriteListings.ts
+++ app/actions/getFavoriteListings.ts
@@ -1,8 +1,6 @@
 import prisma from "@/app/libs/prismadb"
 import getCurrentUser from "./getCurrentUser"
 
-
-
 export default async function getFavoriteListings() {
     try {
         const currentUser = await getCurrentUser();
